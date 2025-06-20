bada97e413ba65be654e2ca5fafc4d2c20711c9d
Biyao Zhang <zby2022@login38.chn.perlmutter.nersc.gov>
2025-06-20 08:40:20 -0700

    “Initialize”

--- Estimator/mml_3d_prediction.py
+++ Estimator/mml_3d_prediction.py
@@ -3,9 +3,7 @@ import importlib
 import tools
 from predictor import Predictor
 
-
 import argparse
-
 
 def get_layer_input_shape(encoder_config, module_name, function_name):
     module = importlib.import_module(module_name)
@@ -17,12 +15,11 @@ def get_map_function(module_name, function_name):
     return getattr(module, function_name)
 
 
-def get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs, save_folder, save_name):
+def get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs):
 
     pp, mp, dp, b, h, l, dim, steps_per_update, gpus_per_node = training_config
     encoder_config = mp, b, h, l, dim
     head_encoder_layers, middle_encoder_layers, tail_encoder_layers = encoder_layers_list
-
 
     operator_config_folder = operator_data_folder
     nccl_config_folder = nccl_data_folder
@@ -35,10 +32,6 @@ def get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data
     propagation_list = ['fwd', 'bwd']
 
     run_time_dict = {}
-
-    save_path = save_folder + '/' + save_name + '.csv'
-
-    portion_save_path = save_folder + '/' + save_name + '_module_portion' + '.csv'
 
     portion_columns = ['module', 'portion']
 
@@ -57,16 +50,12 @@ def get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data
 
             run_time_dict[writing_name] = prediction
             
-            tools.write_one_result_to_csv(save_path, columns_name, [writing_name, shape_new, prediction])
-
 
     pp_p2p_cost = predictor.pp_p2p(nccl_data_folder, nccl_config_folder, nccl_gpu_name, b*l*dim, mp, dp, pp, gpus_per_node) if pp > 1 else 0
     run_time_dict['pp_p2p'] = pp_p2p_cost
-    tools.write_one_result_to_csv(save_path, columns_name, ['pp_p2p', [b*l*dim//mp], pp_p2p_cost])
 
     mp_allreduce_cost = predictor.mp_allreduce(nccl_data_folder, nccl_config_folder, nccl_gpu_name, b*l*dim, mp, gpus_per_node) if mp > 1 else 0
     run_time_dict['mp_allreduce'] = mp_allreduce_cost
-    tools.write_one_result_to_csv(save_path, columns_name, ['mp_allreduce', [b*l*dim], mp_allreduce_cost])
 
     head_parameters = get_embedding_parameters(encoder_config) + head_encoder_layers * get_encoder_parameters(encoder_config)
     middle_parameters = middle_encoder_layers * get_encoder_parameters(encoder_config)
@@ -76,25 +65,19 @@ def get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data
     middle_dp_allreduce_cost = dp_allreduce_cost(predictor, middle_parameters, mp, dp, gpus_per_node, comm_bucket, nccl_data_folder, nccl_config_folder, nccl_gpu_name) if dp > 1 else 0
     tail_dp_allreduce_cost = dp_allreduce_cost(predictor, tail_parameters, mp, dp, gpus_per_node, comm_bucket, nccl_data_folder, nccl_config_folder, nccl_gpu_name) if dp > 1 else 0
     run_time_dict['dp_allreduce'] = [head_dp_allreduce_cost, middle_dp_allreduce_cost, tail_dp_allreduce_cost]
-    tools.write_one_result_to_csv(save_path, columns_name, ['dp_allreduce', [head_parameters, middle_parameters, tail_parameters], [head_dp_allreduce_cost, middle_dp_allreduce_cost, tail_dp_allreduce_cost]])
 
     head_dp_allgather_cost = dp_allgather_cost(predictor, head_parameters, mp, dp, gpus_per_node, comm_bucket, nccl_data_folder, nccl_config_folder, nccl_gpu_name) if dp > 1 else 0
     middle_dp_allgather_cost = dp_allgather_cost(predictor, middle_parameters, mp, dp, gpus_per_node, comm_bucket, nccl_data_folder, nccl_config_folder, nccl_gpu_name) if dp > 1 else 0
     tail_dp_allgather_cost = dp_allgather_cost(predictor, tail_parameters, mp, dp, gpus_per_node, comm_bucket, nccl_data_folder, nccl_config_folder, nccl_gpu_name) if dp > 1 else 0
     run_time_dict['dp_allgather'] = [head_dp_allgather_cost, middle_dp_allgather_cost, tail_dp_allgather_cost]
-    tools.write_one_result_to_csv(save_path, columns_name, ['dp_allgather', [head_parameters//dp, middle_parameters//dp, tail_parameters//dp], [head_dp_allgather_cost, middle_dp_allgather_cost, tail_dp_allgather_cost]])
 
     head_dp_optimizer_cost = local_update_cost(predictor, operator_data_folder, operator_config_folder, gpu_name, mp, head_encoder_layers, precision, dim, 'firstStage_optimizer') if dp > 1 else 0
     middle_dp_optimizer_cost = local_update_cost(predictor, operator_data_folder, operator_config_folder, gpu_name, mp, middle_encoder_layers, precision, dim, 'middleStage_optimizer') if dp > 1 else 0
     tail_dp_optimizer_cost = local_update_cost(predictor, operator_data_folder, operator_config_folder, gpu_name, mp, tail_encoder_layers, precision, dim, 'lastStage_optimizer') if dp > 1 else 0
     run_time_dict['optimizer'] = [head_dp_optimizer_cost, middle_dp_optimizer_cost, tail_dp_optimizer_cost]
-    tools.write_one_result_to_csv(save_path, columns_name, ['optimizer', [head_parameters, middle_parameters, tail_parameters], run_time_dict['optimizer']])
 
     run_time_dict['update'] = [a + b for a, b in zip(run_time_dict['dp_allgather'], [head_dp_optimizer_cost, middle_dp_optimizer_cost, tail_dp_optimizer_cost])]
-    tools.write_one_result_to_csv(save_path, columns_name, ['update', [head_parameters, middle_parameters, tail_parameters], run_time_dict['update']])
     
-
-    # encoder_function_list = ['layernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'layernorm', 'linear3', 'gelu', 'linear4']
     encoder_fwd = 0
     encoder_bwd = 0
     for function in encoder_function_list:
@@ -107,9 +90,7 @@ def get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data
     encoder_bwd += encoder_fwd
     
     run_time_dict['encoder_fwd'] = encoder_fwd
-    tools.write_one_result_to_csv(save_path, columns_name, ['encoder_fwd', encoder_config, encoder_fwd])
     run_time_dict['encoder_bwd'] = encoder_bwd
-    tools.write_one_result_to_csv(save_path, columns_name, ['encoder_bwd', encoder_config, encoder_bwd])
 
 
     head_fwd = run_time_dict.get('embedding_fwd') + mp_allreduce_cost + run_time_dict.get('encoder_fwd') * head_encoder_layers
@@ -121,35 +102,11 @@ def get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data
     tail_bwd = run_time_dict.get('encoder_bwd') * tail_encoder_layers + run_time_dict.get(layernorm_name + '_bwd') + run_time_dict.get('linear_final_bwd') + run_time_dict.get('parallel_cross_entropy_128_bwd')
 
     run_time_dict['fwd'] = [head_fwd, middle_fwd, tail_fwd]
-    tools.write_one_result_to_csv(save_path, columns_name, ['stage_fwd', encoder_config, [head_fwd, middle_fwd, tail_fwd]])
     run_time_dict['bwd'] = [head_bwd, middle_bwd, tail_bwd]
-    tools.write_one_result_to_csv(save_path, columns_name, ['stage_bwd', encoder_config, [head_bwd, middle_bwd, tail_bwd]])
 
-
-    # update_noninterleaved_1F1B = noninterleaved_1F1B([head_fwd+pp_p2p_cost, middle_fwd+pp_p2p_cost, tail_fwd], [head_bwd, middle_bwd+pp_p2p_cost, tail_bwd+pp_p2p_cost], steps_per_update, pp) + head_dp_optimizer_cost + head_dp_allreduce_cost + head_dp_allgather_cost
-    # tools.write_one_result_to_csv(save_path, columns_name, ['noninterleaved_1F1B', training_config, update_noninterleaved_1F1B])
-    
     update_all_F_all_B_max_optimizer = all_F_all_B([head_fwd+pp_p2p_cost, middle_fwd+pp_p2p_cost, tail_fwd], [head_bwd, middle_bwd+pp_p2p_cost, tail_bwd+pp_p2p_cost], steps_per_update, pp) + head_dp_allreduce_cost + max(run_time_dict['update'])
-    tools.write_one_result_to_csv(save_path, columns_name, ['all_F_all_B_max_optimizer', training_config, update_all_F_all_B_max_optimizer])
 
-
-    # update_all_F_all_B_head = all_F_all_B([head_fwd+pp_p2p_cost, middle_fwd+pp_p2p_cost, tail_fwd], [head_bwd, middle_bwd+pp_p2p_cost, tail_bwd+pp_p2p_cost], steps_per_update, pp) +  + head_dp_optimizer_cost + head_dp_allreduce_cost + head_dp_allgather_cost
-    # tools.write_one_result_to_csv(save_path, columns_name, ['all_F_all_B_head', training_config, update_all_F_all_B_head])
-
-
-
-
-
-
-    # head_bwd_after_pp = head_dp_optimizer_cost + head_dp_allreduce_cost + head_dp_allgather_cost
-    # last_middle_bwd_after_pp = middle_dp_optimizer_cost + middle_dp_allreduce_cost + middle_dp_allgather_cost - head_bwd 
-    # tail_bwd_after_pp = tail_dp_optimizer_cost + tail_dp_allreduce_cost + tail_dp_allgather_cost - (pp-2)*(middle_bwd+pp_p2p_cost) - head_bwd 
-    # tools.write_one_result_to_csv(save_path, columns_name, ['after_pp', training_config, [head_bwd_after_pp, last_middle_bwd_after_pp, tail_bwd_after_pp]])
-
-    
-    # max_after_pp= max([head_bwd_after_pp, last_middle_bwd_after_pp, tail_bwd_after_pp])
-    # update_all_F_all_B_max = all_F_all_B([head_fwd+pp_p2p_cost, middle_fwd+pp_p2p_cost, tail_fwd], [head_bwd, middle_bwd+pp_p2p_cost, tail_bwd+pp_p2p_cost], steps_per_update, pp) + max_after_pp 
-    # tools.write_one_result_to_csv(save_path, columns_name, ['all_F_all_B_max', training_config, update_all_F_all_B_max])
+    return update_all_F_all_B_max_optimizer
 
 
 
@@ -205,10 +162,6 @@ def all_F_all_B(fwd_list, bwd_list, steps_per_update, pp):
     return cost 
 
 
-# def all_F_all_B(fwd_list, bwd_list, steps_per_update, pp):
-#     cost = (steps_per_update - 1) * (max(fwd_list) + max(bwd_list)) + (pp - 3) * (fwd_list[1] + bwd_list[1]) + (sum(fwd_list) + sum(bwd_list))
-#     return cost 
-
 def get_layer_norm_parameteres(encoder_config):
     mp, b, h, l, dim = encoder_config
 
@@ -247,105 +200,14 @@ def get_final_lnear_parameters(encoder_config):
     return partition_vocab_size*dim
 
 
+def pp_partitions(encoders, pp):
+    layers = encoders + 5
+    return [math.ceil(layers/pp)-2, math.floor(layers/pp), math.floor(layers/pp)-3]
 
 
+def one_batch_predict(configs_path):
+    configs = tools.config_decoder(config_path)
 
-def GPT_20B_4_4_8_128(predictor, gpus_per_node, gpu_name, operator_data_folder, nccl_data_folder, save_folder, save_name):
-    # pp, mp, dp, b, h, l, dim, steps_per_update, gpus_per_node
-    training_config = [4, 4, 8, 4, 64, 2048, 6144, 16, gpus_per_node]
-    comm_bucket = 1260000000
-
-    function_list = ['embedding', 'layernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'linear3', 'gelu', 'linear4', 'linear_final', 'parallel_cross_entropy_128', 'res_add']
-    encoder_function_list = ['layernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'layernorm', 'res_add', 'linear3', 'gelu', 'linear4', 'res_add']
-    layernorm_name = 'layernorm'
-    fwd_syncs = 1
-    bwd_syncs = 2
-    
-    # Partitioning pipeline stages with method type:transformer|mlp
-    encoder_layers_list = [11, 12, 9]
-    get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs, save_folder, save_name)
-
-
-
-def GPT_20B_4_8_4_128(predictor, gpus_per_node, gpu_name, operator_data_folder, nccl_data_folder, save_folder, save_name):
-    # pp, mp, dp, b, h, l, dim, steps_per_update, gpus_per_node
-    training_config = [4, 8, 4, 4, 64, 2048, 6144, 16, gpus_per_node]
-    comm_bucket = 1260000000
-
-    function_list = ['embedding', 'layernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'linear3', 'gelu', 'linear4', 'linear_final', 'parallel_cross_entropy_128', 'res_add']
-    encoder_function_list = ['layernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'layernorm', 'res_add', 'linear3', 'gelu', 'linear4', 'res_add']
-    layernorm_name = 'layernorm'
-    fwd_syncs = 1
-    bwd_syncs = 2
-    
-    # Partitioning pipeline stages with method type:transformer|mlp
-    encoder_layers_list = [11, 12, 9]
-    get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs, save_folder, save_name)
-
-
-
-def GPT_20B_8_4_4_128(predictor, gpus_per_node, gpu_name, operator_data_folder, nccl_data_folder, save_folder, save_name):
-    # pp, mp, dp, b, h, l, dim, steps_per_update, gpus_per_node
-    training_config = [8, 4, 4, 4, 64, 2048, 6144, 16, gpus_per_node]
-    comm_bucket = 1260000000
-
-    function_list = ['embedding', 'layernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'linear3', 'gelu', 'linear4', 'linear_final', 'parallel_cross_entropy_128', 'res_add']
-    encoder_function_list = ['layernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'layernorm', 'res_add', 'linear3', 'gelu', 'linear4', 'res_add']
-    layernorm_name = 'layernorm'
-    fwd_syncs = 1
-    bwd_syncs = 2
-    
-    # Partitioning pipeline stages with method type:transformer|mlp
-    encoder_layers_list = [5, 6, 3]
-    get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs, save_folder, save_name)
-
-
-
-def llama_13B_4_8_2_64(predictor, gpus_per_node, gpu_name, operator_data_folder, nccl_data_folder, save_folder, save_name):
-    # pp, mp, dp, b, h, l, dim, steps_per_update, gpus_per_node
-    training_config = [4, 8, 2, 4, 40, 2048, 5120, 16, gpus_per_node]
-    comm_bucket = 500000000
-
-    function_list = ['embedding', 'RMSlayernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'linear3', 'gelu', 'linear4', 'linear_final', 'parallel_cross_entropy_128', 'res_add']
-    encoder_function_list = ['RMSlayernorm', 'linear1', 'RoPE', 'baddbmm', 'ScaledUpperTriangMaskedSoftmax', 'bmm', 'linear2', 'RMSlayernorm', 'res_add', 'linear3', 'gelu', 'linear4', 'res_add']
-    layernorm_name = 'RMSlayernorm'
-
-    fwd_syncs = 2
-    bwd_syncs = 2
-
-    #Partitioning pipeline stages with method type:transformer|mlp
-    encoder_layers_list = [10, 11, 8]
-    get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs, save_folder, save_name)
-
-
-
-def llemma_7B_4_2_2_16(predictor, gpus_per_node, gpu_name, operator_data_folder, nccl_data_folder, save_folder, save_name):
-    # pp, mp, dp, b, h, l, dim, steps_per_update, gpus_per_node
-    training_config = [4, 2, 2, 4, 32, 4096, 4096, 8, gpus_per_node]
-    comm_bucket = 1260000000
-
-    function_list = ['embedding', 'RMSlayernorm', 'linear1', 'RoPE', 'flash_atten', 'linear2', 'linear3', 'gelu', 'linear4', 'linear_final', 'parallel_cross_entropy_128', 'res_add']
-    encoder_function_list = ['RMSlayernorm', 'linear1', 'RoPE', 'flash_atten', 'linear2', 'RMSlayernorm', 'res_add', 'linear3', 'gelu', 'linear4', 'res_add']
-    layernorm_name = 'RMSlayernorm'
-
-    fwd_syncs = 2
-    bwd_syncs = 2
-
-    #Partitioning pipeline stages with method type:transformer|mlp
-    encoder_layers_list = [8, 9, 6]
-    get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs, save_folder, save_name)
-
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config_path", default='', type=str,
-                        help="Path of config yml file")
-    args = parser.parse_args()
-    return args
-
-
-def main(configs):
     predictor = Predictor()
 
     gpu_name = configs['gpu_name']
@@ -365,23 +227,21 @@ def main(configs):
     fwd_syncs = configs['fwd_syncs']
     bwd_syncs = configs['bwd_syncs']
 
-    save_folder = configs['save_folder']
-    save_name = configs['save_name']
-
-    get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs, save_folder, save_name)
+    get_operator_statistics(predictor, gpu_name, operator_data_folder, nccl_data_folder, training_config, comm_bucket, encoder_layers_list, function_list, encoder_function_list, layernorm_name, fwd_syncs, bwd_syncs)
 
 
-def pp_partitions(encoders, pp):
-    layers = encoders + 5
-    return [math.ceil(layers/pp)-2, math.floor(layers/pp), math.floor(layers/pp)-3]
-
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_path", default='', type=str,
+                        help="Path of config yml file")
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
     args = parse_arguments() 
-    configs = tools.config_decoder(args.config_path)
-    main(configs)
-
+    one_batch_timecost = one_batch_predict(args.config_path)
+    print(f'Estimated timecost of current training configs is {one_batch_timecost} us.')
 
 # test_command
 # python mml_3d_prediction.py --config_path ./target_config/llemma_7b_4_2_2_P.yml
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# distributed_training_estimator_of_LLM
+# Distributed Training Estimator of LLMs
 Time cost estimator of LLM's distributed training
+
+- AI4CI
+
+## References
+
+### What can be included:
+- Links to related resources (e.g., libraries, tools) or external documentation.
+- Definitions of key terms and concepts used in the project.
+
+
+
+---
+
+# Tutorials
+
+### What can be included:
+- A high-level introduction to the task or workflow.
+- A list of prerequisites (e.g. software requirements, configurations).
+- A sequence of steps that guide users through accomplishing a goal.
+- Visual aids such as screenshots or GIFs, if necessary, to clarify complex steps.
+- End results, showcasing the outcome of following the tutorial.
+
+---
+
+# How-To Guides
+
+### What can be included:
+- Problem description: A brief overview of the task or issue at hand.
+- Step-by-step instructions on how to complete the task.
+- Potential variations or advanced tips to enhance the process.
+- Troubleshooting advice for common pitfalls.
+- Relevant code snippets, commands, or configuration examples.
+
+
+---
+
+# Explanation
+
+### What can be included:
+- High-level overview of core concepts and principles.
+- Explanations of how the system works and why certain design patterns or approaches were chosen.
+- Diagrams, flowcharts, or illustrations to visualize key concepts.
+- Background information to help users grasp the context of the project’s design and architecture.
+- Suggested readings or resources for further exploration.
+
+
+
+
+
+## Acknowledgements
+
+*National Science Foundation (NSF) funded AI institute for Intelligent Cyberinfrastructure with Computational Learning in the Environment (ICICLE) (OAC 2112606)*
